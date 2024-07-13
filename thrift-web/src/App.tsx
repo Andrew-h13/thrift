9ccd9083fdc54d4c6424d9c0ce321946a9196228
Andrew-h13 <andrew.phi.h13@gmail.com>
2024-07-13 01:15:17 -0500

    Header complete, main body still needs work

--- thrift-web/src/App.tsx
+++ thrift-web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import "boxicons/css/boxicons.min.css";
 
 function App() {
   return (
@@ -7,14 +8,25 @@ function App() {
       <div className="header">
         <div className="hInfo">
           <div className="logo">
-            <label> Website Logo</label>
+            <label> ThriftSea </label>
           </div>
           <div className="user">
-            <button className="logButton">Login</button>
             <button className="logButton"> Sign Up</button>
+            <button className="logButton">Login</button>
             <label className="username">User placeholder</label>
-            <button className="dropCircle"> circle</button>
+            <button className="dropCircle">
+              <i className="bx bxs-tree-alt dropIcon"></i>
+            </button>
           </div>
+        </div>
+      </div>
+      <div className="welcome-banner"></div>
+
+      <div className="main-container">
+        <div className="windows-container">
+          <div className="window-l"></div>
+          <div className="window-m"></div>
+          <div className="window-r"></div>
         </div>
       </div>
     </>
