592cc30ab2d8091eb1bb3b115a197f9c12e38242
Andrew-h13 <andrew.phi.h13@gmail.com>
2024-07-13 20:50:30 -0500

    sidebar: complete front page: almost need some tweaks

--- thrift-web/src/App.tsx
+++ thrift-web/src/App.tsx
@@ -11,20 +11,43 @@ function App() {
             <label>ThriftSea</label>
           </div>
           <div className="user">
-            <button className="logButton">Sign Up</button>
-            <button className="logButton">Login</button>
+            {/* <button className="logButton">Sign Up</button>
+            <button className="logButton">Login</button> */}
           </div>
         </div>
       </div>
       <div className="content">
-        <div className="welcome-banner">Welcome to the Banner</div>
+        <div className="welcome-banner">
+          <h1 className="header"> Welcome to ThriftSea</h1>
+          <p className="description">
+            At ThriftSea, we dive deep to bring you treasures without sinking
+            your budget. Where thrifty meets the sea of endless possibilities!
+          </p>
+          <div className="button-container">
+            <button className="Join"> Join For Free</button>
+            <button className="Sign"> Sign In</button>
+          </div>
+        </div>
+        <div className="spacer1"></div>
         <div className="cat-container">
           <div className="windows-container">
-            <div className="window-l">Left Window</div>
-            <div className="window-m">Middle Window</div>
-            <div className="window-r">Right Window</div>
-            <div className="window-m">Middle Window</div>
-            <div className="window-r">Right Window</div>
+            <div className="window-l">
+              <div className="picture-window"></div>
+              <div className="pic-spacer"></div>
+              <h1 className="cat-name">Electronics</h1>
+            </div>
+            <div className="window-m">
+              <div className="picture-window"></div>
+              <div className="pic-spacer"></div>
+
+              <h1 className="cat-name">Clothing</h1>
+            </div>
+            <div className="window-r">
+              <div className="picture-window"></div>
+              <div className="pic-spacer"></div>
+
+              <h1 className="cat-name">Toys</h1>
+            </div>
           </div>
         </div>
       </div>
